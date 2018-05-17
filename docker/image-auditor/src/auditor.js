6dd1bf0b711860cdee0c3ce7ab3bc2e8dc32641e
romain_silvestri <romain.silvestri@heig-vd.ch>
2018-05-17 14:22:14 +0200

    validation OK

--- docker/image-auditor/src/auditor.js
+++ docker/image-auditor/src/auditor.js
@@ -8,49 +8,49 @@ const TIME_OUT = 5000;
 
 var s = dgram.createSocket('udp4');
 
-var musicianMap = new Map();
+// If we use a map instead, validation fails so we go back to arrays
+var musicianArray = [];
 
 // Bind the server to the multicast address so it can listen for messages on it
-s.bind(PORT, function(){
-   console.log("The auditor is now in the multicast group on port " + PORT);
-   s.addMembership(ADDRESS);
+s.bind(PORT, function () {
+    console.log("The auditor is now in the multicast group on port " + PORT);
+    s.addMembership(ADDRESS);
 });
 
 // Define what we do when a message is received
-s.on('message', function(msg, src){
-   console.log("A message has been received " + msg);
+s.on('message', function (msg, src) {
+    console.log("The message " + msg + " has been received from " + src);
 
-   // Create a musician from the received message
-   var receivedMusician = JSON.parse(msg);
+    // Create a musician from the received message
+    var receivedMusician = JSON.parse(msg);
 
     // We check if we already received a message from this musician
-    if(musicianMap.has(receivedMusician.uuid)){
-        // if it's the case, we update the timestamp
-        musicianMap.get(receivedMusician.uuid).lastMessage = moment();
-    }else{
-        // Otherwise, we create a new musician with the received one informations
-        var newMusician = new Object();
-        newMusician.uuid = receivedMusician.uuid;
-        newMusician.instrument = receivedMusician.instrument;
-        newMusician.sound = receivedMusician.sound;
-        newMusician.lastMessage = moment();
-        newMusician.firstMessage = moment();
-        musicianMap.set(newMusician.uuid, newMusician);
+    for (var i = 0; i < musicianArray.length; i++) {
+        if (musicianArray[i].uuid == receivedMusician.uuid) {
+            musicianArray[i].lastMessage = moment();
+            return;
+        }
     }
+
+    // If the musician is not in the array
+    receivedMusician.lastMessage = moment();
+    musicianArray.push(receivedMusician);
 });
 
 // Creating the TCP server
-var serverTCP = net.createServer(function(socket){
-    musicianMap.forEach(function(item, key, musicianMap){
-        // If the musician hasn't played for TIME_OUT ms, delete it from the map
-        if(moment().diff(item.lastMessage) > TIME_OUT ){
-            console.log(JSON.stringify(item) + " is being deleted.")
-            musicianMap.delete(key);
+var serverTCP = net.createServer(function (socket) {
+    for (var i = 0; i < musicianArray.length; i++) {
+        // If the musician hasn't played for TIME_OUT ms, delete it from the array
+        if (moment().diff(musicianArray[i].lastMessage) > TIME_OUT) {
+            console.log(JSON.stringify(musicianArray[i]) + " is being deleted.")
+            musicianArray.splice(i, 1);
+
         }
-    });
-    socket.write(JSON.stringify(musicianMap));
+    }
+
+    socket.write(JSON.stringify(musicianArray));
     socket.end();
-}
+});
 
 // Making our server listening on the right port
 serverTCP.listen(PORT);
